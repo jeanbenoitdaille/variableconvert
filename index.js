3c20a5526a5edd24a8007de684e1778ff02acacc
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-06 12:12:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Ne modifiez pas ces lignes
+let nombre_1 = 15;  
+let nombre_2 = "30";
+ 
+// Écrivez votre code et complétez les lignes ci-dessous
+nombre_2 = Number(nombre_2);
+let resultat = nombre_1 + nombre_2;
